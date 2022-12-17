0cfc015e0e76e57ca7c4c228886c23661d454049
AC <X00165424@mytudublin.ie>
2022-12-17 23:10:05 +0000

    formatting changes

--- js/grade.js
+++ js/grade.js
@@ -4,10 +4,10 @@ function displayGrades() {
   var selectedModule = this.value;
   var gradeDisplay = document.getElementById('grade-display');
   if (selectedModule === 'Maths') {
-    gradeDisplay.innerHTML = '<h3>Computing Maths Grades</h3><p>Grade 1: A</p><p>Grade 2: A</p><p>Grade 3: A</p>';
+    gradeDisplay.innerHTML = '<h3>Computing Maths Grades</h3><p>Year 1: A</p><p>Year 2: A</p><p>Year 3: A</p>';
   } else if (selectedModule === 'German') {
-    gradeDisplay.innerHTML = '<h3>German Grades</h3><p>Grade 1: A</p><p>Grade 2: B+</p><p>Grade 3: B</p>';
+    gradeDisplay.innerHTML = '<h3>German Grades</h3><p>Year 1: A</p><p>Year 2: B+</p><p>Year 3: B</p>';
   } else if (selectedModule === 'Software Development') {
-    gradeDisplay.innerHTML = '<h3>Software Development Grades</h3><p>Grade 1: C+</p><p>Grade 2: B+</p><p>Grade 3: C</p>';
+    gradeDisplay.innerHTML = '<h3>Software Development Grades</h3><p>Year 1: C+</p><p>Year 2: B+</p><p>Year 3: C</p>';
   }
 }
